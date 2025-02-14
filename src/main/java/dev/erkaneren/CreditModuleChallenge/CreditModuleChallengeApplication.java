360dd72165244e4eb081d9d328df45afabaced0e
Erkan EREN <git@erkaneren.dev>
2025-02-14 10:27:59 +0000

    spring initializr

---
+++ src/main/java/dev/erkaneren/CreditModuleChallenge/CreditModuleChallengeApplication.java
@@ -0,0 +1,13 @@
+package dev.erkaneren.CreditModuleChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditModuleChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditModuleChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/erkaneren/CreditModuleChallenge/CreditModuleChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.erkaneren.CreditModuleChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditModuleChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
